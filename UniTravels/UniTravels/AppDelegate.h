e3f35ac86b5d74cb9494d15decd8fec465a09587
Gloria <gjchen@ucdavis.edu>
2015-08-11 13:57:11 -0700

    initial commit

---
+++ UniTravels/UniTravels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UniTravels
+//
+//  Created by Gloria Chen on 8/11/15.
+//  Copyright (c) 2015 Gloria Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UniTravels/UniTravels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniTravels
+//
+//  Created by Gloria Chen on 8/11/15.
+//  Copyright (c) 2015 Gloria Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
